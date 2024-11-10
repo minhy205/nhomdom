68a0132e26b65fe772e400dc44024f45e5573c2e
Lê Anh Kiệt <lekiet.24102002@gmail.com>
2024-11-10 11:28:33 +0700

    back end licsumuahang

---
+++ Koi.WebApplication/Models/licsumuahang.cs
@@ -0,0 +1,13 @@
+﻿namespace Koi.WebApplication.Models
+{
+    public class licsumuahang
+    {
+        public int ProductId { get; set; }  // ID sản phẩm
+        public string ProductName { get; set; }  // Tên sản phẩm
+        public decimal Price { get; set; }  // Giá sản phẩm
+        public int Quantity { get; set; }  // Số lượng sản phẩm
+        public string Status { get; set; }  // Trạng thái sản phẩm (đã mua, đang chờ,...)
+        public string ImageUrl { get; set; }  // Địa chỉ URL của hình ảnh sản phẩm
+
+    }
+}
