14866f9de294562efe44f2201d06ff902179572b
Gia Nguyễn Trần Huỳnh <gianguyen150605@gmail.com>
2024-11-16 00:36:44 +0700

    Sửa BE login register

--- Koi.Repositories/Entities/KoifarmContext.cs
+++ Koi.Repositories/Entities/KoifarmContext.cs
@@ -34,10 +34,7 @@ public partial class KoifarmContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=KoiStore10;Persist Security Info=True;User ID=sa;Password=123456aA@$;MultipleActiveResultSets=True;TrustServerCertificate=True");
-
+ 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<CaKoi>(entity =>
