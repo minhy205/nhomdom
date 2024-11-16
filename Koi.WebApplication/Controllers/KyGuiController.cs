4093d6bc442fb815284b6f635217422ee6b56989
minhy205 <huylm1640@ut.edu.vn>
2024-11-17 01:09:20 +0700

    fix đường dẫn

--- Koi.WebApplication/Controllers/KyGuiController.cs
+++ Koi.WebApplication/Controllers/KyGuiController.cs
@@ -1,44 +1,49 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
-using Koi.Repositories.Entities;
-using Koi.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc;
+﻿//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Koi.Repositories.Entities;
+//using Koi.Services.Interfaces;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace Koi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class KyGuiController : ControllerBase
-    {
-        private readonly IKyGuiService _kyGuiService;
+//namespace Koi.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class KyGuiController : ControllerBase
+//    {
+//        private readonly IKyGuiService _kyGuiService;
 
-        public KyGuiController(IKyGuiService kyGuiService)
-        {
-            _kyGuiService = kyGuiService;
-        }
+//        public KyGuiController(IKyGuiService kyGuiService)
+//        {
+//            _kyGuiService = kyGuiService;
+//        }
 
-        // GET: api/KyGui/{kyGuiID}
-        [HttpGet("{kyGuiID}")]
-        public async Task<ActionResult<KyGui>> GetByIdAsync(int kyGuiID)
-        {
-            var result = await _kyGuiService.GetByIdAsync(kyGuiID);
-            if (result == null)
-            {
-                return NotFound();
-            }
-            return Ok(result);
-        }       
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
 
-        // DELETE: api/KyGui/{kyGuiID}
-        [HttpDelete("{kyGuiID}")]
-        public async Task<IActionResult> DeleteAsync(int kyGuiID)
-        {
-            var success = await _kyGuiService.DeleteAsync(kyGuiID);
-            if (!success)
-            {
-                return NotFound();
-            }
-            return NoContent();
-        }
-    }
-}
+//        // GET: api/KyGui/{kyGuiID}
+//        [HttpGet("{kyGuiID}")]
+//        public async Task<ActionResult<KyGui>> GetByIdAsync(int kyGuiID)
+//        {
+//            var result = await _kyGuiService.GetByIdAsync(kyGuiID);
+//            if (result == null)
+//            {
+//                return NotFound();
+//            }
+//            return Ok(result);
+//        }       
+
+//        // DELETE: api/KyGui/{kyGuiID}
+//        [HttpDelete("{kyGuiID}")]
+//        public async Task<IActionResult> DeleteAsync(int kyGuiID)
+//        {
+//            var success = await _kyGuiService.DeleteAsync(kyGuiID);
+//            if (!success)
+//            {
+//                return NotFound();
+//            }
+//            return NoContent();
+//        }
+//    }
+//}
--- Koi.WebApplication/Controllers/LichSuKyGuiController.cs
+++ Koi.WebApplication/Controllers/LichSuKyGuiController.cs
@@ -1,43 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Koi.Services.Interfaces;
-using Koi.Repositories.Entities;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Koi.Services.Interfaces;
+//using Koi.Repositories.Entities;
+//using System.Threading.Tasks;
+//using System.Collections.Generic;
 
-namespace Koi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class LichSuKyGuiController : ControllerBase
-    {
-        private readonly ILichSuKyGuiService _lichSuKyGuiService;
+//namespace Koi.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class LichSuKyGuiController : ControllerBase
+//    {
+//        private readonly ILichSuKyGuiService _lichSuKyGuiService;
 
-        public LichSuKyGuiController(ILichSuKyGuiService lichSuKyGuiService)
-        {
-            _lichSuKyGuiService = lichSuKyGuiService;
-        }
+//        public LichSuKyGuiController(ILichSuKyGuiService lichSuKyGuiService)
+//        {
+//            _lichSuKyGuiService = lichSuKyGuiService;
+//        }
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult<LichSuKyGui>> GetByIdAsync(int id)
-        {
-            var result = await _lichSuKyGuiService.GetByIdAsync(id);
-            if (result == null) return NotFound();
-            return Ok(result);
-        }
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
 
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<LichSuKyGui>>> GetAllAsync()
-        {
-            var result = await _lichSuKyGuiService.GetAllAsync();
-            return Ok(result);
-        }
+//        [HttpGet("{id}")]
+//        public async Task<ActionResult<LichSuKyGui>> GetByIdAsync(int id)
+//        {
+//            var result = await _lichSuKyGuiService.GetByIdAsync(id);
+//            if (result == null) return NotFound();
+//            return Ok(result);
+//        }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAsync(int id)
-        {
-            var success = await _lichSuKyGuiService.DeleteAsync(id);
-            if (!success) return NotFound();
-            return NoContent();
-        }
-    }
-}
+//        [HttpGet]
+//        public async Task<ActionResult<IEnumerable<LichSuKyGui>>> GetAllAsync()
+//        {
+//            var result = await _lichSuKyGuiService.GetAllAsync();
+//            return Ok(result);
+//        }
+
+//        [HttpDelete("{id}")]
+//        public async Task<IActionResult> DeleteAsync(int id)
+//        {
+//            var success = await _lichSuKyGuiService.DeleteAsync(id);
+//            if (!success) return NotFound();
+//            return NoContent();
+//        }
+//    }
+//}
